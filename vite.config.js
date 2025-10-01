ce6105b2d0e564383d67662e947579869c44ef68
Amankumar <amanks2580@gmail.com>
2025-10-01 22:55:33 +0530

    deployed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Canva-Stencil-editor/',  // 👈 use your repo name here
 })
